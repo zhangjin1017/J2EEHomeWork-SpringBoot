a2890926e008220a29b75c5243d6d2ec696af885
zj <1216916137@qq.com>
2022-04-20 12:19:48 +0800

    Initial commit

---
+++ src/test/java/com/zj/j2eehomeworkspringboot/J2EeHomeWorkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zj.j2eehomeworkspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2EeHomeWorkSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
