e009df88fc5b343af31e06c2022186486dfc0055
zj <1216916137@qq.com>
2022-04-25 10:24:45 +0800

    test1

--- src/main/java/com/zj/controller/ControllerTest.java
+++ src/main/java/com/zj/controller/ControllerTest.java
@@ -1,11 +1,30 @@
 package com.zj.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/test")
 public class ControllerTest {
-
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello 江苏海洋大学";
+    }
+    @RequestMapping("/t1")
+    public String test() {
+        System.out.println("t1------");
+        System.out.println("t1------");
+        System.out.println("t1------");
+        return "t1";
+    }
+    @PostMapping("/t2/{name}")
+    public String test2(@PathVariable("name") String name) {
+        System.out.println("t2------");
+        System.out.println(name+"------");
+        System.out.println("t2------");
+        return "t2";
+    }
 }
 
